d1e10dbfda6297be00138052abbfad161e1435be
paunovgeorgi <gmaisonp@gmail.com>
2023-09-18 18:54:42 +0300

    next addition

---
+++ 02. Multidimensional Arrays/Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+int rows = matrixSize[0];
+int cols = matrixSize[1];
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = data[col];
+    }
+}
+
+for (int col = 0; col < cols; col++)
+{
+    int sum = 0;
+    for (int row = 0; row < rows; row++)
+    {
+        sum += matrix[row, col];
+    }
+    Console.WriteLine(sum);
+}
