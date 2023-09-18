b5a6ff4991ce36b0f15bca6e2f03937d3c8b7d8d
paunovgeorgi <gmaisonp@gmail.com>
2023-09-18 18:52:15 +0300

    first solution

---
+++ 02. Multidimensional Arrays/Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+int rows = matrixSize[0];
+int cols = matrixSize[1];
+int[,] matrix = new int[rows, cols];
+int sum = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    int[] data = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        sum += (matrix[row, col] = data[col]);
+    }
+}
+
+Console.WriteLine(rows);
+Console.WriteLine(cols);
+Console.WriteLine(sum);
