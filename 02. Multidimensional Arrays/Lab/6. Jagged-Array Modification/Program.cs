6ef627bab6e36cb67ba187dcabb0b2bb016d2075
paunovgeorgi <gmaisonp@gmail.com>
2023-09-18 19:00:11 +0300

    Multidimensional Arrays

---
+++ 02. Multidimensional Arrays/Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,47 @@
+﻿int[][] jaggedArray = new int[int.Parse(Console.ReadLine())][];
+for (int row = 0; row < jaggedArray.Length; row++)
+{
+    jaggedArray[row] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+}
+
+string input = string.Empty;
+
+while ((input = Console.ReadLine()) != "END")
+{
+    string[] tokens = input.Split();
+    string command = tokens[0];
+    switch (command)
+    {
+        case "Add":
+            int row = int.Parse(tokens[1]);
+            int col = int.Parse(tokens[2]);
+            int value = int.Parse(tokens[3]);
+            if ((row >= 0 && row < jaggedArray.Length) && (col >= 0 && col < jaggedArray[row].Length))
+            {
+                jaggedArray[row][col] += value;
+            }
+            else
+            {
+                Console.WriteLine("Invalid coordinates");
+            }
+            break;
+        case "Subtract":
+            row = int.Parse(tokens[1]);
+            col = int.Parse(tokens[2]);
+            value = int.Parse(tokens[3]);
+            if ((row >= 0 && row < jaggedArray.Length) && (col >= 0 && col < jaggedArray[row].Length))
+            {
+            jaggedArray[row][col] -= value;
+            }
+            else
+            {
+                Console.WriteLine("Invalid coordinates");
+            }
+            break;
+    }
+}
+
+foreach (int[] array in jaggedArray)
+{
+    Console.WriteLine(string.Join(" ", array));
+}
---
+++ 02. Multidimensional Arrays/Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+long[][] jagged = new long[n][];
+jagged[0] = new long[1] { 1 };
+
+for (int row = 1; row < n; row++)
+{
+    jagged[row] = new long[row + 1];
+
+    for (int col = 0; col < jagged[row].Length; col++)
+    {
+        if (jagged[row - 1].Length > col)
+        {
+            jagged[row][col] += jagged[row - 1][col];
+        }
+        if (col > 0)
+        {
+            jagged[row][col] += jagged[row - 1][col - 1];
+        }
+    }
+}
+
+foreach (long[] array in jagged)
+{
+    Console.WriteLine(string.Join(" ", array));
+}
