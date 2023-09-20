29017abb696a184f796a4fb69da5416099e6fcce
paunovgeorgi <gmaisonp@gmail.com>
2023-09-20 15:50:43 +0300

    Sets and Dictionaries

---
+++ 03. Sets and Dictionaries Advanced/Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,30 @@
+﻿string input;
+HashSet<string> cars = new HashSet<string>();
+while ((input = Console.ReadLine()) != "END")
+{
+    string[] tokens = input.Split(", ");
+    string command = tokens[0];
+    string car = tokens[1];
+
+    if (command == "IN")
+    {
+        cars.Add(car);
+    }
+    else
+    {
+        cars.Remove(car);
+    }
+}
+
+if (cars.Any())
+{
+foreach (var car in cars)
+{
+    Console.WriteLine(car);
+}
+}
+
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
---
+++ 03. Sets and Dictionaries Advanced/Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,45 @@
+﻿string input; 
+HashSet<string> normal = new HashSet<string>();
+HashSet<string> vip = new HashSet<string>();
+
+    while ((input = Console.ReadLine()) != "PARTY")
+    {
+        if (input[0] >= '0' && input[0] <= '9')
+    {
+        vip.Add(input);
+    }
+    else
+    {
+        normal.Add(input);
+    }
+}
+
+while ((input = Console.ReadLine()) != "END")
+{
+    if (normal.Contains(input))
+        {
+            normal.Remove(input);
+        }
+        else if (vip.Contains(input))
+        {
+            vip.Remove(input);
+        }
+}
+
+Console.WriteLine(normal.Count + vip.Count);
+
+   if (vip.Any())
+   {
+       foreach (var person in vip)
+       {
+           Console.WriteLine(person);
+       }
+   }
+
+   if (normal.Any())
+   {
+       foreach (var person in normal)
+       {
+           Console.WriteLine(person);
+       }
+   }
