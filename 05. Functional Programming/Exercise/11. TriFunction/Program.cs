7d8d22d0c53124e502fbe1cd6378b60d6e169638
paunovgeorgi <gmaisonp@gmail.com>
2023-10-17 16:03:15 +0300

    Functional Programming

---
+++ 05. Functional Programming/Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿int num = int.Parse(Console.ReadLine());
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Func<string, int, bool> traverse = (name, num) =>
+{
+    int sum = name.Sum(x => x);
+    return sum >= num;
+};
+
+Func<Func<string, int, bool>, string[], int, string> output = (func, array, num) =>
+{
+    foreach (string name in array)
+    {
+        if (func(name, num))
+        {
+            return name;
+        }
+
+    }
+
+    return null;
+};
+
+Console.WriteLine(output(traverse, names, num));
