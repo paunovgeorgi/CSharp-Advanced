3f3593e966f0573c8fa8ad4454a07d01dbd8662f
paunovgeorgi <gmaisonp@gmail.com>
2023-10-22 10:59:05 +0300

    Iterators and Comparators

---
+++ 10. Iterators and Comparators/04.Froggy/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Lake lake = new(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
